d3f3904b835ccf861f82144281f2ffa0b08843d9
Jesse Sopel <jesse.sopel@wazo.io>
2023-05-24 15:36:53 -0400

    Also add some typing

--- wazo_asterisk_doc_extractor/main.py
+++ wazo_asterisk_doc_extractor/main.py
@@ -1,9 +1,11 @@
 # Copyright 2020-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+from __future__ import annotations
 
 import json
 import argparse
 from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
 
 tag_to_quote = [
     "emphasis",
@@ -14,42 +16,42 @@ tag_to_quote = [
 ]
 
 
-def trim_spaces(s):
+def trim_spaces(s: str) -> str:
     # Turn all kind of white spaces to a single one
     return " ".join(s.split())
 
 
-def reformat_block(s):
+def reformat_block(string: str) -> str:
     # Replace some tags by quotes and remove all other tags
     for tag in tag_to_quote:
-        s = s.replace(f"<{tag}>", '"')
-        s = s.replace(f"</{tag}>", '"')
-    elem = ElementTree.fromstring(s)
-    s = ElementTree.tostring(elem, encoding="utf8", method="text").decode("utf8")
-    return trim_spaces(s)
+        string = string.replace(f"<{tag}>", '"')
+        string = string.replace(f"</{tag}>", '"')
+    elem = ElementTree.fromstring(string)
+    string = ElementTree.tostring(elem, encoding="utf-8", method="text").decode("utf-8")
+    return trim_spaces(string)
 
 
-def extract_para(elem):
+def extract_para(elem: Element) -> str:
     parts = [
-        reformat_block(ElementTree.tostring(para, encoding="utf8").decode("utf8"))
+        reformat_block(ElementTree.tostring(para, encoding="utf-8").decode("utf-8"))
         for para in elem.findall("para")
     ]
     return "\n".join(parts)
 
 
-def extract_node(elem):
+def extract_node(elem: Element) -> str:
     notes = [extract_para(note) for note in elem.findall("note")]
     return "\n".join(notes)
 
 
-def extract_choices(elem):
+def extract_choices(elem: Element) -> dict[str, str]:
     return {
         enum.attrib["name"]: extract_para(enum) if enum.text else ""
         for enum in elem.findall("./*enum")
     }
 
 
-def extract_pjsip_option(elem):
+def extract_pjsip_option(elem: Element) -> dict[str, str]:
     synopsis, description, note = "", "", ""
     choices = {}
 
@@ -71,7 +73,7 @@ def extract_pjsip_option(elem):
     }
 
 
-def extract_pjsip_doc_section(elem):
+def extract_pjsip_doc_section(elem: Element) -> dict[str, dict[str, str]]:
     return {
         option.attrib["name"]: extract_pjsip_option(option)
         for option in elem
@@ -79,7 +81,7 @@ def extract_pjsip_doc_section(elem):
     }
 
 
-def extract_pjsip_doc(root):
+def extract_pjsip_doc(root: Element) -> dict[str, dict[str, dict[str, str]]]:
     sections = root.findall(".//*[@name='res_pjsip']/configFile/")
     return {
         section.attrib["name"]: extract_pjsip_doc_section(section)
@@ -87,7 +89,7 @@ def extract_pjsip_doc(root):
     }
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("file")
     args = parser.parse_args()
